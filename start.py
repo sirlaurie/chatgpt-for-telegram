ee2d9f7336c31d593d30d8f1c64ceb5e1300f11a
sirlaurie <liusuzhang@gmail.com>
2023-02-18 01:48:13 +0800

    enhanced bot with menu

--- start.py
+++ start.py
@@ -5,7 +5,6 @@
 
 import logging
 import os
-from typing import List
 
 from telegram import __version__ as TG_VER
 
@@ -21,7 +20,6 @@ if __version_info__ < (20, 0, 0, "alpha", 5):
 
 
 from telegram import Update
-
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -31,7 +29,19 @@ from telegram.ext import (
 )
 
 import httpx
+
 from allowed import user_list
+from handlers import (
+    handler,
+    linux_terminal_handler,
+    translator_handler,
+    rewrite_handler,
+    code_helper_handler,
+    cyber_secrity_handler,
+    etymologists_handler,
+    genius_handler,
+    advanced_frontend_handler,
+)
 
 # Enable logging
 logging.basicConfig(
@@ -40,13 +50,11 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-bot_token = "6187271186:AAGB4Am0DBGoAvm-WIiOvxE9kHJ6Ic0dmFQ"
-openai_apikey = os.environ.get("openai_apikey")
-openai_model = "text-davinci-003"
-max_tokens = 2048
-temperature = 0.3
-
-conversations: List[dict[str, str | int]] = [{"user_id": user_id} for user_id in user_list]
+bot_token: str = os.environ.get("bot_token", "")
+openai_apikey: str = os.environ.get("openai_apikey", "")
+openai_model: str = os.environ.get("openai_model", "")
+max_tokens: int = int(os.environ.get("max_tokens", "2048"))
+temperature: float = float(os.environ.get("temperature", "0.6"))
 
 
 def allowed(user_id):
@@ -59,36 +67,45 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         # user = update.effective_user
         await update.message.reply_text(text="Hello, 你先说")
     else:
-        await update.message.reply_text(text=f"你没有权限访问此bot.请将你的id {update.message.chat.id} 发送给管理员, 等待批准")
+        await update.message.reply_text(
+            text=f"你没有权限访问此bot.请将你的id {context._user_id} 发送给管理员, 等待批准"
+        )
 
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # 调用模型
     text = ""
-    current_user_id = update.message.from_user.id
-    if not allowed(current_user_id):
-        await update.message.reply_text(text=f"你没有权限访问此bot.请将你的id {update.message.chat.id} 发送给管理员, 等待批准")
-        return
+    conversation_id = (
+        context.chat_data.get("conversation_id", None)
+        if isinstance(context.chat_data, dict)
+        else None
+    )
+    parent_message_id = (
+        context.chat_data.get("parent_message_id", None)
+        if isinstance(context.chat_data, dict)
+        else None
+    )
 
-    user_conversation = {}
-    for conversation in conversations:
-        if current_user_id == conversation["user_id"]:
-            user_conversation = conversation
-    conversation_id = user_conversation.get("conversation_id", None)
-    parent_message_id = user_conversation.get("parent_message_id", None)
+    data = {
+        "message": update.message.text,
+        "conversationId": conversation_id,
+        "parentMessageId": parent_message_id,
+    }
 
-    data = {"message": update.message.text, "conversationId": conversation_id, "parentMessageId": parent_message_id}
-
-    await update.get_bot().send_chat_action(update.message.chat.id, "typing", read_timeout=10)
+    # 调用模型
     async with httpx.AsyncClient() as client:
         while not text:
             try:
-                response = await client.post("http://git.lloring.com:5000/conversation",
-                    json=data,
-                    timeout=60)
+                await update.get_bot().send_chat_action(
+                    update.message.chat.id, "typing", write_timeout=20000
+                )
+
+                response = await client.post(
+                    "http://git.lloring.com:5000/conversation", json=data, timeout=60
+                )
                 resp = response.json()
-                user_conversation["conversation_id"] = resp["conversationId"]
-                user_conversation["parent_message_id"] = resp["messageId"]
+                if isinstance(context.chat_data, dict):
+                    context.chat_data["conversation_id"] = resp["conversationId"]
+                    context.chat_data["parent_message_id"] = resp["messageId"]
                 text = resp["response"]
                 if text:
                     break
@@ -104,6 +121,14 @@ def main() -> None:
     application = Application.builder().token(bot_token).build()
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler(translator_handler, handler(translator_handler))) # type: ignore
+    application.add_handler(CommandHandler(linux_terminal_handler, handler(linux_terminal_handler))) # type: ignore
+    application.add_handler(CommandHandler(rewrite_handler, handler(rewrite_handler))) # type: ignore
+    application.add_handler(CommandHandler(code_helper_handler, handler(code_helper_handler))) # type: ignore
+    application.add_handler(CommandHandler(cyber_secrity_handler, handler(cyber_secrity_handler))) # type: ignore
+    application.add_handler(CommandHandler(etymologists_handler, handler(etymologists_handler))) # type: ignore
+    application.add_handler(CommandHandler(genius_handler, handler(genius_handler))) # type: ignore
+    application.add_handler(CommandHandler(advanced_frontend_handler, handler(advanced_frontend_handler))) # type: ignore
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
 
     # Run the bot until the user presses Ctrl-C
