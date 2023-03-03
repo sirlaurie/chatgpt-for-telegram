5c4ddc19682a1895301eca7c3384d76fa0f849f0
sirlaurie <liusuzhang@gmail.com>
2023-03-03 23:58:16 +0800

    add new prompt

--- src/handlers.py
+++ src/handlers.py
@@ -12,6 +12,7 @@ from utils import waring, apply_to_prove
 
 reset_handler = "reset"
 genius_handler = "genius"
+expand_handler = "expand"
 rewrite_handler = "rewrite"
 translator_handler = "translator"
 code_helper_handler = "code_helper"
@@ -20,6 +21,7 @@ cyber_secrity_handler = "cyber_secrity"
 linux_terminal_handler = "linux_terminal"
 advanced_frontend_handler = "advanced_frontend"
 
+expand = "我希望你充当一个文字工作者, 你在后面会收到我提供给你的一小段文字, 你的职责是根据这段文字扩充一下, 但保持相同的意思, 尽量多的使用中国政府机关常用的文书和表达方式, 尽量扩充到800字左右, 保持语句通顺, 逻辑通畅. 让别人不易察觉这段文字是由AI生成的."
 genius = "我希望你充当IT专家. 我会向您提供有关我的技术问题所需的所有信息, 而您的职责是解决我的问题. 你应该使用你的计算机科学、网络基础设施和 IT 安全知识来解决我的问题. 在您的回答中使用适合所有级别的人的智能、简单和易于理解的语言将很有帮助. 用要点逐步解释您的解决方案很有帮助. 尽量避免过多的技术细节,但在必要时使用它们. 我希望您回复解决方案,而不是写任何解释."
 rewrite = "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. "
 translator = "我想让你充当私人翻译, 我会用任何语言与你交谈, 你将自动检测语言, 并始终翻译为中文. 我希望你可以使用比较高级的词语, 使他们更文艺, 但保持相同的意思. 不要写任何解释."
@@ -48,6 +50,8 @@ def pick(act: str):
             return etymologists
         case "/genius":
             return genius
+        case "/expand":
+            return expand
         case "/advanced_frontend":
             return advanced_frontend
         case "/reset":
@@ -72,6 +76,7 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/cyber_secrity",
         "/etymologists",
         "/genius",
+        "/expand",
         "/advanced_frontend",
         "/reset",
     ]:
